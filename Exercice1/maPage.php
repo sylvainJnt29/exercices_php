4412fd9f7ec04c30939c424e1b297263e7c0ee01
sylvainJnt29 <sylvain.jnt@gmail.com>
2020-11-03 09:58:45 +0100

    premier commit

---
+++ Exercice1/maPage.php
@@ -0,0 +1,12 @@
+<?php
+
+$nomDuJoueur1 = "Jhon";
+$ageDuJoueur1 = 21;
+$estUnHomme = true;
+
+echo "Nom du joueur 1 : " . $nomDuJoueur1 . "<br>" . "Age du joueur 1 : "  . $ageDuJoueur1 . "<hr>";
+
+$ageDuJoueur1 = $ageDuJoueur1 +1;
+$nomDuJoueur1 = "John";
+
+echo "Nom du joueur 1 : " . $nomDuJoueur1 . "<br>" . "Age du joueur 1 : "  . $ageDuJoueur1 . "<hr>";
